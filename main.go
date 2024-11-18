f0b5214a37d7521eb43cc36d3d66496defea69c6
srk24 <25836646+srk24@users.noreply.github.com>
2024-11-18 19:38:25 +0800

    add apple system rule to cn

--- main.go
+++ main.go
@@ -50,6 +50,32 @@ func main() {
 		"umengcloud.com",
 	}
 
+	// from surge system rule
+	apple_system_domain := []string{
+		"api.smoot.apple.com",
+		"captive.apple.com",
+		"xp.apple.com",
+		"configuration.apple.com",
+		"guzzoni.apple.com",
+		"smp-device-content.apple.com",
+		"aod.itunes.apple.com",
+		"mesu.apple.com",
+		"api.smoot.apple.cn",
+		"gs-loc.apple.com",
+		"mvod.itunes.apple.com",
+		"streamingaudio.itunes.apple.com",
+	}
+
+	// from surge system rule
+	apple_system_domain_suffix := []string{
+		"ess.apple.com",
+		"push-apple.com.akadns.net",
+		"push.apple.com",
+		"lcdn-locator.apple.com",
+		"lcdn-registration.apple.com",
+		"ls.apple.com",
+	}
+
 	_ = os.MkdirAll("./sing/tmp/", 0777)
 
 	CompileSingboxFile("./sing/ruleset/process_direct.json")
@@ -89,6 +115,16 @@ func main() {
 	f, _ = Download("https://github.com/SagerNet/sing-geosite/raw/rule-set/geosite-geolocation-cn.srs", "./sing/tmp/geosite-geolocation-cn.srs")
 	DecompileSingboxFile(f.Name())
 	domain, domain_suffix, domain_keyword, _ = UnmarshalSingboxSourceCfg("./sing/tmp/geosite-geolocation-cn.json")
+	for _, item := range apple_system_domain {
+		if exist := slices.Contains(domain, item); !exist {
+			domain = append(domain, item)
+		}
+	}
+	for _, item := range apple_system_domain_suffix {
+		if exist := slices.Contains(domain_suffix, item); !exist {
+			domain_suffix = append(domain_suffix, item)
+		}
+	}
 	GenerateSurgeFile("./surge/list/geolocation-cn.list", domain, domain_suffix)
 	GenerateClashFile("./clash/provider/geolocation-cn.yaml", domain, domain_suffix)
 	GenerateQuanXFile("./quanx/list/geolocation-cn.snippet", domain, domain_suffix, domain_keyword)
@@ -109,13 +145,13 @@ func UnmarshalSingboxSourceCfg(path string) (domain, domain_suffix, domain_keywo
 		for _, item := range rules.Rules[i].Domain {
 			if _, v := domain_map[item]; !v {
 				domain_map[item] = true
-				domain = append(domain, item)
+				domain = append(domain, strings.TrimLeft(item, "."))
 			}
 		}
 		for _, item := range rules.Rules[i].DomainSuffix {
 			if _, v := domain_suffix_map[item]; !v {
 				domain_suffix_map[item] = true
-				domain_suffix = append(domain_suffix, item)
+				domain_suffix = append(domain_suffix, strings.TrimLeft(item, "."))
 			}
 		}
 		for _, item := range rules.Rules[i].DomainKeyword {
@@ -172,7 +208,7 @@ func GenerateQuanXFile(filename string, domain, domainSuffix, domainKeyword []st
 	}
 
 	for _, s := range domainSuffix {
-		_, _ = f.WriteString("host-suffix, " + strings.TrimPrefix(s, ".") + d)
+		_, _ = f.WriteString("host-suffix, " + s + d)
 	}
 
 	for _, s := range domainKeyword {
